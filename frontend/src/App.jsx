9434256029b21992306cc8e5c21a6505f6bdfc78
arjunbisht471 <142390689+arjunbisht471@users.noreply.github.com>
2024-12-01 09:26:05 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,7 +55,7 @@ function App() {
   // Function to start chat
   const startChat = () => {
     if (username.trim()) {
-      const socket = new WebSocket("ws://localhost:5000");
+     const socket = new WebSocket("ws://perfactchat.com:5000");
       setWs(socket);
       setIsChatting(true);
     } else {
